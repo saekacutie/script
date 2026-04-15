90e0048b1510ca4c0f6c406fddbccf628194293a
くんジェット <130436764+saekacutie@users.noreply.github.com>
2026-04-16 02:23:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
-LISTENING_PORT=${PORT:-8080}
+# Overwrite the default 8080 with the GCP-assigned port
+sed -i "s/8080/$PORT/g" /etc/xray/config.json
 
-# Replace the placeholder 9999 with the Cloud Run port
-sed -i "s/9999/$LISTENING_PORT/" /etc/xray/config.json
-
-# PRE-FLIGHT CHECK: Stop if the config is broken
-/usr/bin/xray test -c /etc/xray/config.json
-if [ $? -ne 0 ]; then
-    echo "FATAL: JSON Syntax Error detected."
-    exit 1
-fi
-
-echo "SYSTEM: Starting Xray on Port $LISTENING_PORT..."
-exec /usr/bin/xray run -c /etc/xray/config.json
+echo "Starting Xray on Dynamic Port: $PORT"
+/usr/bin/xray run -c /etc/xray/config.json
