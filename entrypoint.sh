cb7b70a3b13d00b4f4434aa620817039825f85e1
くんジェット <130436764+saekacutie@users.noreply.github.com>
2026-04-15 08:50:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# Overwrite the default 8080 with the GCP-assigned port
+# Replace ALL occurrences of 8080 with the Cloud Run assigned $PORT
 sed -i "s/8080/$PORT/g" /etc/xray/config.json
 
 echo "Starting Xray on Dynamic Port: $PORT"
-/usr/bin/xray run -c /etc/xray/config.json
+exec /usr/bin/xray run -c /etc/xray/config.json
