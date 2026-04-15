a3df6f877172ace442726f4a5abaaa67f91a708a
くんジェット <130436764+saekacutie@users.noreply.github.com>
2026-04-15 20:00:37 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -206,6 +206,7 @@ gcloud run deploy "$SERVICE_NAME" \
     --port 8080 \
     --cpu 2 \
     --memory 4Gi \
+    --cpu-boost \
     --concurrency 1000 \
     --timeout 3600 \
     --min-instances 1 \
