2769731578e86f20143a693926359e26b035bc54
くんジェット <130436764+saekacutie@users.noreply.github.com>
2026-04-17 02:17:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# Overwrite the default 8080 with the GCP-assigned port
+# Inject the dynamic GCP port into config
 sed -i "s/8080/$PORT/g" /etc/xray/config.json
 
-echo "Starting Xray on Dynamic Port: $PORT"
-/usr/bin/xray run -c /etc/xray/config.json
+echo "System Keep-Alive: Active"
+exec /usr/bin/xray run -c /etc/xray/config.json
