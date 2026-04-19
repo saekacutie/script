59f92dab4f57391fa3a06267d4a6d1b5f18b3cb4
くんジェット <130436764+saekacutie@users.noreply.github.com>
2026-04-19 17:00:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-# Inject the dynamic GCP port into config
-sed -i "s/8080/$PORT/g" /etc/xray/config.json
-
-echo "System Keep-Alive: Active"
+python3 /usr/bin/server.py &
 exec /usr/bin/xray run -c /etc/xray/config.json
